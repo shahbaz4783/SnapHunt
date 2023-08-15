536b957a5dd817092a7b3696d9b6f637c0c1aa39
shahbaz4783 <shahbaz4783@gmail.com>
2023-08-15 22:27:36 +0530

    extra code remove

--- src/script.js
+++ src/script.js
@@ -71,18 +71,3 @@ fetchAPIdata(searchTerm)
 	.catch((error) => {
 		console.log(error);
 	});
-
-
-    const header = document.querySelector('header');
-const swipeThreshold = window.innerHeight * 0.15;
-
-const changeBg = () => {
-	if (window.scrollY > swipeThreshold) {
-		header.classList.add('background');
-	} else {
-		header.classList.remove('background');
-	}
-};
-window.addEventListener('scroll', changeBg);
-
-
